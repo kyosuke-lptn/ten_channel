44237fd9b09f34bac31ec3b6486a5e041a00b42f
kyosuke <sspvt065@gmail.com>
2019-11-21 10:23:13 +0900

    add rspec and pry

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,17 @@ module TenChannel
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.test_framework  :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      g.system_tests false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
   end
 end
