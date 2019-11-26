edead556fed5f8bd455d80299389ec0a360a8514
kyosuke <sspvt065@gmail.com>
2019-11-26 11:31:02 +0900

    add #text_match(posting thread)

--- app/models/posting_thread.rb
+++ app/models/posting_thread.rb
@@ -10,4 +10,15 @@ class PostingThread < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, length: { maximum: 500 }
   validates :user_id, presence: true
+
+  def self.text_match(words)
+    return [] if words.blank?
+    regexp = "%#{words.strip.gsub(/[ 　\t]+/, "%")}%"
+    threads = PostingThread.
+      joins(:comments).
+      where("#{PostingThread.table_name}.title Like :this OR #{PostingThread.table_name}.description Like :this OR #{Comment.table_name}.content Like :this", this: regexp).
+      select("#{PostingThread.table_name}.*, count(#{Comment.table_name}.id) as comments_count").
+      group(:id).
+      order("comments_count desc")
+  end
 end
--- spec/models/posting_thread_spec.rb
+++ spec/models/posting_thread_spec.rb
@@ -22,4 +22,30 @@ RSpec.describe PostingThread, type: :model do
       expect(test.posting_thread).to eq category.posting_threads.first
     end
   end
+
+  describe "#text_match" do
+    let!(:name_posting_thread) { create(:posting_thread, title: "match-text") }
+    let!(:name_comments10) { create_list(:comment, 3, posting_thread_id: name_posting_thread.id) }
+    let!(:description_posting_thread) { create(:posting_thread, description: "aaaaa match-text aaaa") }
+    let!(:description_comments5) { create_list(:comment, 2, posting_thread_id: description_posting_thread.id) }
+    let!(:comment_posting_thread) { create(:posting_thread) }
+    let!(:comment_comments1) { create(:comment, content: "match-text", posting_thread_id: comment_posting_thread.id) }
+    let!(:no_match_posting_thread) { create(:posting_thread, title: "no!!!", description: "no!!!") }
+    let!(:no_matoch_comments) { create_list(:comment, 4, content: "no!!!!!!", posting_thread_id: no_match_posting_thread.id) }
+    let!(:threads) { PostingThread.text_match("match-text") }
+
+    it "コメント数の多いthreadが一番に取得できる" do
+      expect(threads[0]).to eq name_posting_thread
+    end
+
+    it "コメント数の少ないthreadが最後に取得できる" do
+      expect(threads[-1]).to eq comment_posting_thread
+    end
+
+    it "検索したワードを含まない物は入っていない" do
+      expect(threads).not_to include [no_match_posting_thread]
+    end
+  end
+
+
 end
